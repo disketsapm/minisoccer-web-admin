2d90d2960281935fc9a8ee08af0e6bbb93e48ffe
deprasny <deprasny@gmail.com>
2024-02-03 09:58:22 +0700

    Add new files and components

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Header from '@/components/shared/header';
+import Sidebar from '@/components/shared/sidebar/sidebar';
+import { redirect, usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const protectedRoutes = ['/', '/dashboard'];
+  let isAuthenticated: boolean;
+
+  if (typeof localStorage !== 'undefined') {
+    isAuthenticated = !!localStorage.getItem('token');
+  } else {
+    isAuthenticated = false;
+  }
+
+  useEffect(() => {
+    if (!isAuthenticated && protectedRoutes.includes(pathname)) {
+      redirect('/login');
+    }
+  }, [isAuthenticated]);
+
+  return (
+    <div className="flex min-h-screen flex-col bg-background">
+      <Sidebar />
+      <div className={`duration-300 flex-1 flex-col space-y-5 ml-[280px]`}>
+        <Header />
+        <main className="flex-1 mx-5 pb-10">{children}</main>
+      </div>
+    </div>
+  );
+}
---
+++ components/shared/sidebar/sidebar.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { LayoutDashboard, Settings } from 'lucide-react';
+import SidebarHeader from './sidebar-header';
+import SidebarMenu from './sidebar-menu';
+import { useSidebar } from '@/store/sidebar-store';
+
+const Sidebar = () => {
+  const { isOpen } = useSidebar();
+  const menus = [
+    {
+      title: 'Dashboard',
+      icon: <LayoutDashboard className="text-white text-3xl rounded cursor-pointer" />,
+      link: '/dashboard',
+    },
+    {
+      title: 'Access Control',
+      icon: <Settings className="text-white text-3xl rounded cursor-pointer" />,
+      submenu: [
+        {
+          title: 'Role',
+          link: '/access_control/role',
+        },
+        {
+          title: 'Account',
+          link: '/access_control/account',
+        },
+        {
+          title: 'Menu',
+          link: '/access_control/menu',
+        },
+        {
+          title: 'Profile',
+          link: '/access_control/profile',
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className="fixed z-10">
+      <div className={`w-[280px] bg-sidebar h-screen duration-300 relative z-10`}>
+        <div className="flex flex-col justify-between h-full overflow-y-auto">
+          <div className="flex flex-col justify-center">
+            <SidebarHeader props={isOpen} />
+            {menus.map((menu, index) => (
+              <SidebarMenu
+                key={index}
+                menu={menu}
+                isOpen={isOpen}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
